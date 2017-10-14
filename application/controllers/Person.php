8f784eddc72bd8148c16f9d44df229beb7fffe65
Syed NAzir Hussain <syednazir13@gmail.com>
2017-10-13 22:24:53 -0700

    remove ordinary code

--- application/controllers/Person.php
+++ application/controllers/Person.php
@@ -3,55 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class Person extends CI_Controller{
-	
-	public function Index(){
-		echo "iam in home controller index method";
-	}
-	public function Name($name){
-		echo "Hey : ".$name;;
-	}
-/*	public function Add($num1,$num2){
-		echo $num2+$num1;
-	}*/
-	public function sendMail(){
-
-		$this->load->view("contact");
-
-		$this->load->library('email');
-
-		$this->email->from($this->input->post('email'), $this->input->post('name'));
-		$this->email->to('someone@example.com');
-
-
-		$this->email->subject('Email Test');
-		$this->email->message($this->input->post('message'));
-
-		#$this->email->send();
-
-		if ( $this->email->send() ) {
-			echo $this->email->send();
-		}else{
-			echo $this->email->print_debugger();
-		}
-	}
 
 	public function subject(){
-		//echo print_r($this->person->getAllSubject());die();
 		$result['subject'] = $this->person->getAllSubject();
 		$this->load->view('person/subject',$result);
-		
 	}
 
 	public function edit($id){
-		//echo site_url();
-		//echo base_url();
-		//$id = $this->uri->segment(2);
-		//echo $id;die();
-		
 		$result['subject'] = $this->person->getSubjectById($id);
-
 		$this->load->view('person/edit',$result);
-		
 	}
 
 	public function update(){
